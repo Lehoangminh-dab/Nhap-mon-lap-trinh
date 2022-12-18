725725a2579d9c5e9dee7c7beaed2d94bbf9bcbd
Lehoangminh-dab <71586973+Lehoangminh-dab@users.noreply.github.com>
2022-12-18 10:04:10 +0700

    Code hom KT giua ki của Nguyễn Văn Hưng

--- xoa_phan_tu.cpp
+++ xoa_phan_tu.cpp
@@ -35,7 +35,8 @@ int** inputMatrix(int nRows, int nCols){
 void printMatrix(int** matrix, int nRows, int nCols)
 {
     for ( int i=0 ; i < nRows ; i++)
-        {for ( int j=0; j < nCols; j++)
+        {
+        for ( int j=0; j < nCols; j++)
             cout << matrix[i][j] << " ";
         cout << endl;}
 } 
